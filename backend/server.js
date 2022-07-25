c2052de95c654d555a98abed8b4113f9b331ae15
Vishwanath <vishwanathnaga2112.com>
2022-07-25 19:36:07 +0530

    First Commit

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
+mongoose.connect(uri);
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
@@ -23,6 +22,7 @@ const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
